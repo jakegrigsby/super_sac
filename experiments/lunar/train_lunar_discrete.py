1a7fa3eaad19c4283b29134812852c134e1a54c0
jakegrigsby <jcg6dn@virginia.edu>
2021-03-23 17:10:04 -0400

    update lunar script

--- experiments/lunar/train_lunar_discrete.py
+++ experiments/lunar/train_lunar_discrete.py
@@ -5,12 +5,12 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-import afbc
-from afbc.wrappers import SimpleGymWrapper, DiscreteActionWrapper
+import uafbc
+from uafbc.wrappers import SimpleGymWrapper, DiscreteActionWrapper
 import gym
 
 
-class IdentityEncoder(afbc.nets.AFBCEncoder):
+class IdentityEncoder(uafbc.nets.Encoder):
     def __init__(self, obs_dim):
         super().__init__()
         self._dim = obs_dim
@@ -31,11 +31,11 @@ def train_gym_discrete(args):
     action_space = test_env.action_space
 
     # create agent
-    agent = afbc.AFBCAgent(
+    agent = uafbc.Agent(
         action_space.n,
         encoder=IdentityEncoder(state_space.shape[0]),
-        actor_network_cls=afbc.nets.mlps.DiscreteActor,
-        critic_network_cls=afbc.nets.mlps.DiscreteCritic,
+        actor_network_cls=uafbc.nets.mlps.DiscreteActor,
+        critic_network_cls=uafbc.nets.mlps.DiscreteCritic,
         hidden_size=128,
         discrete=True,
         critic_ensemble_size=2,
@@ -44,10 +44,10 @@ def train_gym_discrete(args):
     )
 
     # create replay buffer
-    buffer = afbc.replay.PrioritizedReplayBuffer(size=250_000)
+    buffer = uafbc.replay.PrioritizedReplayBuffer(size=250_000)
 
     # run training
-    afbc.afbc(
+    uafbc.uafbc(
         agent=agent,
         train_env=train_env,
         test_env=test_env,
@@ -58,23 +58,24 @@ def train_gym_discrete(args):
         use_bc_update_online=False,
         actor_lr=1e-4,
         critic_lr=1e-4,
-        batch_size=128,
+        batch_size=256,
         num_steps_offline=0,
         num_steps_online=250_000,
         random_warmup_steps=10_000,
         max_episode_steps=1000,
         weighted_bellman_temp=None,
         weight_type=None,
-        encoder_lambda=0.,
-        actor_lambda=0.,
-        aug_mix=1.,
+        encoder_lambda=0.0,
+        actor_lambda=0.0,
+        aug_mix=1.0,
         augmenter=None,
+        pop=False,
     )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--env", type=str, default="LunarLander-v2")
-    parser.add_argument("--name", type=str, default="afbc_online_discrete")
+    parser.add_argument("--name", type=str, default="uafbc_online_discrete")
     args = parser.parse_args()
     train_gym_discrete(args)
