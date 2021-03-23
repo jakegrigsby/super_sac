bd28350bdcb6b51707c8a6f576ab5f7ec6a20e6f
jakegrigsby <jcg6dn@virginia.edu>
2021-03-23 15:09:51 -0400

    remove afbc again

--- afbc/afbc/envs.py
+++
@@ -1,9 +0,0 @@
-import gym
-
-
-class SimpleGymWrapper(gym.wrappers.ObservationWrapper):
-    def __init__(self, env):
-        super().__init__(env)
-
-    def observation(self, obs):
-        return {"obs": obs}
